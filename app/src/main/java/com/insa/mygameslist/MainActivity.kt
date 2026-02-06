7871568d7f84654b575d7a0f555a2f099be1f7a5
mathis-r <98423827+mathis-r@users.noreply.github.com>
2026-02-06 16:54:26 +0100

    fin du TP2

--- app/src/main/java/com/insa/mygameslist/MainActivity.kt
+++ app/src/main/java/com/insa/mygameslist/MainActivity.kt
@@ -4,17 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
 import com.insa.mygameslist.data.IGDB
 import com.insa.mygameslist.ui.theme.MyGamesListTheme
 
@@ -43,10 +56,48 @@ class MainActivity : ComponentActivity() {
                     contentWindowInsets = WindowInsets.systemBars,
                     modifier = Modifier.fillMaxSize()
                 ) { innerPadding ->
+                    LazyColumn(
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        //Text("À remplir", modifier = Modifier.padding(innerPadding))
 
-                    Text("À remplir", modifier = Modifier.padding(innerPadding))
-
+                        for (i in 0..<IGDB.games.size) {
+                            item {
+                                GameBox(i, innerPadding)
+                            }
+                        }
+                    }
                 }
+            }
+        }
+    }
+
+    @Composable
+    fun GameBox(nbGame: Int, innerPadding: PaddingValues) {
+        val indeces = IGDB.games[nbGame].genres
+        var genresStr: String = "Genres : "
+        for (i in 0..<indeces.size) {
+            try {
+                genresStr+=IGDB.genres.first { it.id == indeces[i] }.name
+                genresStr+=", "
+            } catch (_: NoSuchElementException){}
+        }
+        val index = IGDB.games[nbGame].cover
+        val coverUrl = "https:"+ IGDB.covers.first { it.id == index }.url
+        Row(
+            modifier = Modifier
+                .padding(12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.End
+        ) {
+            AsyncImage(model=coverUrl, contentDescription = "Cover")
+            Column(
+                modifier = Modifier
+                    .padding(12.dp),
+                Arrangement.SpaceEvenly
+            ) {
+                Text(IGDB.games[nbGame].name)
+                Text(genresStr)
             }
         }
     }
--- app/src/main/java/com/insa/mygameslist/data/IGDB.kt
+++ app/src/main/java/com/insa/mygameslist/data/IGDB.kt
@@ -8,15 +8,57 @@ import com.insa.mygameslist.R
 object IGDB {
 
     lateinit var covers: List<Cover>
-
+    lateinit var games: List<Games>
+    lateinit var genres: List<Genres>
+    lateinit var platforms: List<Platforms>
+    lateinit var platformsLogos: List<PlatformsLogos>
     fun load(context: Context) {
         val coversFromJson: List<Cover> = Gson().fromJson(
             context.resources.openRawResource(R.raw.covers).bufferedReader(),
             object : TypeToken<List<Cover>>() {}.type
         )
-
         covers = coversFromJson
+
+        val genresFromJson: List<Genres> = Gson().fromJson(
+            context.resources.openRawResource(R.raw.genres).bufferedReader(),
+            object : TypeToken<List<Genres>>() {}.type
+        )
+        genres = genresFromJson
+
+        val platformsFromJson: List<Platforms> = Gson().fromJson(
+            context.resources.openRawResource(R.raw.platforms).bufferedReader(),
+            object : TypeToken<List<Platforms>>() {}.type
+        )
+        platforms = platformsFromJson
+
+        val platformsLogosFromJson: List<PlatformsLogos> = Gson().fromJson(
+            context.resources.openRawResource(R.raw.platform_logos).bufferedReader(),
+            object : TypeToken<List<PlatformsLogos>>() {}.type
+        )
+        platformsLogos = platformsLogosFromJson
+
+        val gamesFromJson: List<Games> = Gson().fromJson(
+            context.resources.openRawResource(R.raw.games).bufferedReader(),
+            object : TypeToken<List<Games>>() {}.type
+        )
+
+        games = gamesFromJson
+
     }
 }
 
 data class Cover(val id: Long, val url: String)
+data class Games(
+    val id: Long,
+    val cover: Long,
+    val first_release_date: Long,
+    val genres: List<Int>,
+    val name: String,
+    val platforms: List<Int>,
+    val summary: String,
+    val total_rating: Double
+)
+
+data class Genres(val id: Int, val name: String)
+data class PlatformsLogos(val id: Int, val url: String)
+data class Platforms(val id: Int, val name: String, val url: Int)
